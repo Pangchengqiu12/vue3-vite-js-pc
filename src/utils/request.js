689ba8bb348cc26229893445e15162320efc1c68
li_wei <2212261902@qq,com>
2023-05-26 08:37:18 +0800

    注释取消重复请求的代码

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
 import { useMemberStore } from '@/stores/index'
 
-const pendingRequests = {}
+// const pendingRequests = {}
 const token = useMemberStore().userInfo?.accessToken
-let requestKey = null
-let errorRequest = false //判断重新发送的请求是否失败
+// let requestKey = null
+// let errorRequest = false //判断重新发送的请求是否失败
 let maxCount = 2 //最大重试次数
 
 let request = axios.create({
@@ -23,12 +23,12 @@ request.interceptors.request.use(
     config.method === 'get' || config.method === 'delete'
       ? (config.headers['Content-Type'] = 'application/x-www-form-urlencoded')
       : ''
-    requestKey = config.method + config.url + JSON.stringify(config.params || {})
-    pendingRequests[requestKey] && !errorRequest // 如果请求已经存在且不是失败的请求，则取消请求
-      ? (config.cancelToken = new axios.CancelToken((cancel) => {
-          cancel('请求已取消')
-        }))
-      : ''
+    // requestKey = config.method + config.url + JSON.stringify(config.params || {})
+    // pendingRequests[requestKey] && !errorRequest // 如果请求已经存在且不是失败的请求，则取消请求
+    //   ? (config.cancelToken = new axios.CancelToken((cancel) => {
+    //       cancel('请求已取消')
+    //     }))
+    //   : ''
     return config
   },
   (error) => {
@@ -38,12 +38,12 @@ request.interceptors.request.use(
 )
 request.interceptors.response.use(
   ({ data }) => {
-    errorRequest = false
-    pendingRequests[requestKey] = true
+    // errorRequest = false
+    // pendingRequests[requestKey] = true
     return data
   },
   (error) => {
-    errorRequest = true
+    // errorRequest = true
     maxCount <= 0 ? Promise.reject(error) : (request(error.config), (maxCount -= 1))
   },
   // maxCount <= 0 ? Promise.reject(error) : request(opt, maxCount - 1)
